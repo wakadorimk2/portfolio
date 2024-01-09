44ceafedb345d457f9f519ccae0c439858e2b0ad
wakadorimk2 <moriyamakensetsu@yahoo.co.jp>
2024-01-09 22:55:41 +0900

    作品をtomlファイルにまとめて、それを読み込んで表示するようにした。

--- portfolio/app.py
+++ portfolio/app.py
@@ -1,22 +1,17 @@
+import tomllib
 import streamlit as st
-
-def get_image_urls():
-    image_urls = [
-        'https://pbs.twimg.com/media/E-wmSP8UcBAYX9-?format=jpg&name=4096x4096',
-        'https://pbs.twimg.com/media/FCi6IbpVgAUQUss?format=jpg&name=4096x4096',
-        'https://pbs.twimg.com/media/FI6Nv84akAcB8gA?format=jpg&name=4096x4096',
-        'https://pbs.twimg.com/media/FIL3KXZacAIBv48?format=jpg&name=4096x4096',
-    ]
-    return image_urls
 
 
 def init_page():
     st.set_page_config(
-        page_title='wakadoriのポートフォリオ',
+        page_title='wakadori\'s works',
         page_icon='🐔',
         layout='centered',
-        initial_sidebar_state='expanded',
+        initial_sidebar_state='auto',
     )
+    with open("portfolio/works.toml", "rb") as f:
+        works = tomllib.load(f)
+    st.session_state.works = works["works"]["illustrations"]
 
 
 def step_by_step():
@@ -34,35 +29,17 @@ def step_by_step():
 
     # UIパーツ=================================
     def buttons(now):
-        col = st.columns(3)
-        if ss.now < 3:
-            col[2].button('次へ進む', on_click=countup)
-        if ss.now > 0:
-            col[1].button('前へ戻る', on_click=countdown)
-        if ss.now > 1:
-            col[0].button('はじめから', on_click=reset)
+        col = st.columns(2)
+        col[1].button('次へ進む', on_click=countup)
+        col[0].button('前に戻る', on_click=countdown)
 
     # アプリ本体=================================
-    col = st.columns([2,1])
-    col[0].write('wakadoriのポートフォリオ')
-    if col[1].toggle('1枚ずつ', True):
-        if   ss.now == 0:
-            buttons(ss.now)
-            st.image(get_image_urls()[ss.now])
-        elif ss.now == 1:
-            buttons(ss.now)
-            st.image(get_image_urls()[ss.now])
-        elif ss.now == 2:
-            buttons(ss.now)
-            st.image(get_image_urls()[ss.now])
-        else:
-            st.write('### 完了！')
-            buttons(ss.now)
-            st.success('全てのステップが完了しました')
+    st.title('wakadori\'s works')
+    if   ss.now == 0:
+        for work in ss.works:
+            st.image(work["image_urls"][0])
     else:
-        st.image(get_image_urls()[0])
-        st.image(get_image_urls()[1])
-        st.image(get_image_urls()[2])
+        pass
 
 
 def main():
